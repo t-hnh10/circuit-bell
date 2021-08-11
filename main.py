1e8419c3e4683424c32cc8ae757e1671f7929353
Tony Huynh <huynh.tony55@gmail.com>
2021-08-11 23:15:09 +1000

    Add support for Lilabeauty

--- main.py
+++ main.py
@@ -30,21 +30,37 @@ def stylevana(links, product_list, webhook):
         writer.writerow([datetime.datetime.now(), name, price, link])
     webhook.send('Store: *Stylevana*\n' + product_list)
 
+def lilabeauty(links, product_list, webhook):
+    for link in links:
+        driver.get(link)
+        html_text = driver.page_source
+        soup = BeautifulSoup(html_text, 'lxml')
+        name = soup.find('h1', class_='ProductMeta__Title Heading u-h2').text
+        price = soup.find('span', class_='ProductMeta__Price Price Price--highlight Text--subdued u-h4').text
+        link = soup.find('link', rel='canonical')['href']
+        product_list += response(name, link, price)
+        writer.writerow([datetime.datetime.now(), name, price, link])
+    webhook.send('Store: *Lilabeauty*\n' + product_list)
+
 # HADA LABO
 
 stylevana(config.hada_labo_list, product_list, config.hada_labo_webhook)
+lilabeauty(config.hada_labo_list_lilabeauty, product_list, config.hada_labo_webhook)
 
 # COSRX
 
 stylevana(config.cosrx_list, product_list, config.cosrx_webhook)
+lilabeauty(config.cosrx_list_lilabeauty, product_list, config.cosrx_webhook)
 
 # Purito
 
 stylevana(config.purito_list, product_list, config.purito_webhook)
+lilabeauty(config.purito_list_lilabeauty, product_list, config.purito_webhook)
 
 # Pyunkang Yul
 
 stylevana(config.pyunkang_yul_list, product_list, config.pyunkang_yul_webhook)
+lilabeauty(config.pyunkang_yul_list_lilabeauty, product_list, config.pyunkang_yul_webhook)
 
 f.close()
 driver.quit()
